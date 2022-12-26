c2751c939dbdc0b545eb8f39cc0796c277768851
Gina Beth Russell <ginabethrussell@Ginas-MBP-2.cinci.rr.com>
2022-12-26 15:25:43 -0500

    creates initial test to verify valid and invalid passwords

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,17 +1,9 @@
 // Testing Pure Functions
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
+import {isPasswordAllowed} from '../auth'
 
-// 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
-
-// 🐨 write tests for valid and invalid passwords
-// 💰 here are some you can use:
-//
 // valid:
 // - !aBc123
-//
 // invalid:
 // - a2c! // too short
 // - 123456! // no alphabet characters
@@ -19,3 +11,16 @@ test.todo('remove me')
 // - abc123! // no uppercase letters
 // - ABC123! // no lowercase letters
 // - ABCdef123 // no non-alphanumeric characters
+
+test('valid passwords for isPasswordAllowed', () => {
+  expect(isPasswordAllowed('!aBc123')).toBe(true)
+})
+
+test('invalid passwords for isPasswordAllowed', () => {
+  expect(isPasswordAllowed('a2c!')).toBe(false)
+  expect(isPasswordAllowed('123456!')).toBe(false)
+  expect(isPasswordAllowed('ABCdef!')).toBe(false)
+  expect(isPasswordAllowed('abc123!')).toBe(false)
+  expect(isPasswordAllowed('ABC123!')).toBe(false)
+  expect(isPasswordAllowed('ABCdef123')).toBe(false)
+})
